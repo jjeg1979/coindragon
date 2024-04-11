7eb2060dd8d2154d9dbd3d3ee2f9041ec44b8f6e
jjeg1979 <jjeg1979@gmail.com>
2024-04-11 13:25:34 +0200

    Added Overview to page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import Overview from './components/Overview';
+
 export default function Home() {
   return (
     <main>
       <h2>Portfolio Overview</h2>
+
+      <Overview />
 
       <div className="details">
         <div className="divider"></div>
