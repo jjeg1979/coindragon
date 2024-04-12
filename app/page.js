5ef7dbbe42c69fccb70e0a6c5499783bbe42d632
jjeg1979 <jjeg1979@gmail.com>
2024-04-12 07:38:22 +0200

    Add component Account to page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
+"use client"
+
+import { useState } from 'react';
 import Overview from './components/Overview';
 
 export default function Home() {
+  const [account, setAccount] = useState(null)
+
   return (
     <main>
       <h2>Portfolio Overview</h2>
 
-      <Overview />
+      <Overview account={account} setAccount={setAccount}/>
 
       <div className="details">
         <div className="divider"></div>
