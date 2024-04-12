0cc1d6639414fa7b1a6b540675e9ddaff173b954
jjeg1979 <jjeg1979@gmail.com>
2024-04-12 13:13:55 +0200

    Added Holding Componets

---
+++ app/components/Holdings.js
@@ -0,0 +1,12 @@
+const Holdings = () => {
+    return (
+       <div className="holdings">
+        <h3 className="holdings__title">Asset Holdings</h3>
+        <div className="holdings__chart">
+
+        </div>       
+      </div> 
+    )
+}
+
+export default Holdings;
