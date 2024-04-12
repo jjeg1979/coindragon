e4d74a2667a7943afcd0883540ee7fa8b192570d
jjeg1979 <jjeg1979@gmail.com>
2024-04-12 13:14:05 +0200

    Added Values Component

---
+++ app/components/Values.js
@@ -0,0 +1,13 @@
+const Values = () => {
+    return (
+       <div className="value">
+        <h3 className="value__title">Portfolio 7 Day Value</h3>
+        <div className="value__chart">
+
+        </div>
+
+       </div>
+    )
+}
+
+export default Values;
