62c43c56927657cefd7ab8a90d8d82b5d229eaaf
jjeg1979 <jjeg1979@gmail.com>
2024-04-12 13:00:48 +0200

    Completed Oveview with Total Value and Pct. Change

--- app/components/Overview.js
+++ app/components/Overview.js
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 // Components
@@ -11,7 +11,18 @@ import up from '../assets/up.svg';
 import down from '../assets/down.svg';
 import add from '../assets/add.svg';
 
-const Overview = ({account, setAccount, markets, trackedTokens, setTrackedTokens}) => {
+const Overview = ({
+    account, 
+    setAccount, 
+    markets, 
+    trackedTokens, 
+    setTrackedTokens, 
+    tokens 
+}) => {
+
+    const [value, setValue] = useState(0)
+    const [percentageChange, setPercentageChange] = useState(0.00)
+
 
     const [isAccountModalOpen, setIsAccountModalOpen] = useState(false) // state for Account Modal (true / false)
     const [isAddTokenModalOpen, setIsAddTokenModalOpen] = useState(false)
@@ -26,6 +37,45 @@ const Overview = ({account, setAccount, markets, trackedTokens, setTrackedTokens
             setIsAccountModalOpen(true)
         }
     }
+
+    const calculateValue = () => {
+        let total = 0
+        
+        for (var i = 0; i<tokens.length; i++){
+            if(tokens[i].balance == 0) {continue}
+            total += tokens[i].value
+        }
+        
+        setValue(total)        
+    }
+
+    const calculatePercentageChange = () => {
+        let total = 0
+
+        for (var i = 0; i<tokens.length; i++){
+            if(tokens[i].balance == 0) {continue}
+            
+            // Get past & current values
+            const pastValue = (tokens[i].market.current_price - tokens[i].market.price_change_24h) * tokens[i].balance
+            const currentValue = tokens[i].value            
+            const change = ((currentValue-pastValue)/pastValue) * 100
+
+            total += change
+        }
+
+        setPercentageChange(total)
+
+    }
+
+    useEffect(() => {
+        if(tokens.length === 0) {
+            setValue(0)
+            setPercentageChange(0)
+        } else {
+            calculateValue()
+            calculatePercentageChange()
+        }
+    })
 
     return (
         <div className="overview">
@@ -60,19 +110,21 @@ const Overview = ({account, setAccount, markets, trackedTokens, setTrackedTokens
 
             <div className="overview__total">
                <h3>Total Value</h3> 
-               <p>$0.00</p>
+               <p>{value.toLocaleString('en-US', { style: 'currency', currency: 'USD'})}</p>
             </div>
 
             <div className="overview__change">
                <h3>% Change</h3> 
                <p>
                     <Image
-                        src={up}
+                        src={
+                            percentageChange >= 0 ? up : down
+                        }
                         width={20}
                         height={20}
                         alt="Change direction"
-                    />
-                <span className="green">0.0%</span>
+                    />                  
+                        <span className={percentageChange >= 0 ?'green' : 'red'}>{percentageChange.toFixed(2)}%</span>                        
                </p>
             </div>
 
