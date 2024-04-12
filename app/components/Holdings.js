46091551bb03c7e50b6d560ecce9ff29b145e5ba
jjeg1979 <jjeg1979@gmail.com>
2024-04-12 15:45:51 +0200

    Completed Holdings Component

--- app/components/Holdings.js
+++ app/components/Holdings.js
@@ -1,8 +1,58 @@
-const Holdings = () => {
+import { useState, useEffect } from 'react';
+import Chart from "react-apexcharts";
+const Holdings = ({ tokens }) => {
+
+  const defaulSymbols = ["---", "---", "---", "---", "---"]
+  const defaultBalances = [15.5, 70.1, 35.89, 25, 100.9]
+
+  const [symbols, setSymbols ] = useState(null)
+  const [balances, setBalances ] = useState(null)
+
+  const calculateValue = () => {     
+    let syms = []
+    let bals = []
+
+    for (var i = 0; i < tokens.length; i++) {
+      syms.push(tokens[i].market.symbol.toUpperCase())
+      bals.push(tokens[i].value)
+    }
+    setSymbols(syms)  
+    setBalances(bals)  
+  }
+
+  useEffect(() => {
+    if (tokens.length === 0) {
+      setSymbols(null)
+    } else {
+      setSymbols([])
+      calculateValue()
+    }
+  }, [tokens])
+
     return (
        <div className="holdings">
         <h3 className="holdings__title">Asset Holdings</h3>
         <div className="holdings__chart">
+
+          {console.log(tokens)}
+
+          <Chart 
+            options={{
+              labels: symbols ? symbols : defaulSymbols,
+              legend: {
+                position: 'left',
+                horizontalAlign: 'center',
+                labels: {
+                  fontSize: '48px',
+                  fontWeight: 'bold',
+                  colors: '#ffffff'
+                }
+              }
+            }} 
+            series={balances ? balances : defaultBalances} 
+            type="pie" 
+            height={300} 
+          />
 
         </div>       
       </div> 
